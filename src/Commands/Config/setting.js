835758e0f5ef48881ce6123044d9df8434b9f6c6
rimihaddad2003 <vagbot.discord@gmail.com>
2021-04-09 00:05:23 +0300

    .

--- src/Commands/Config/setting.js
+++ src/Commands/Config/setting.js
@@ -7,8 +7,7 @@ module.exports = {
   args: true,
   cooldown: 3000,
   run: async (client, message, args) => {
-    [setting,
-      ...newset] = args;
+    [setting, ...newset] = args;
     if (setting == "suggestions") {
       if (!newset[0]) {
         message.channel.send(
@@ -18,19 +17,19 @@ module.exports = {
         );
       } else {
         const channel =
-        message.mentions.channels.first() ||
-        message.guild.channels.cache.get(newset[0]);
+          message.mentions.channels.first() ||
+          message.guild.channels.cache.get(newset[0]);
         if (!channel)
           return message.channel.send(
-          "**⚠️ - Invalid channel, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid channel, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("sugg_channel", channel.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set suggestions channel to <#${channel.id}> .**`
-          )
-        );
+          .set("sugg_channel", channel.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set suggestions channel to <#${channel.id}> .**`
+            )
+          );
       }
     }
     if (setting == "logs") {
@@ -42,19 +41,19 @@ module.exports = {
         );
       } else {
         const channel =
-        message.mentions.channels.first() ||
-        message.guild.channels.cache.get(newset[0]);
+          message.mentions.channels.first() ||
+          message.guild.channels.cache.get(newset[0]);
         if (!channel)
           return message.channel.send(
-          "**⚠️ - Invalid channel, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid channel, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("logs_channel", channel.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set logs channel to <#${channel.id}> .**`
-          )
-        );
+          .set("logs_channel", channel.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set logs channel to <#${channel.id}> .**`
+            )
+          );
       }
     }
     if (setting == "welcome") {
@@ -66,19 +65,19 @@ module.exports = {
         );
       } else {
         const channel =
-        message.mentions.channels.first() ||
-        message.guild.channels.cache.get(newset[0]);
+          message.mentions.channels.first() ||
+          message.guild.channels.cache.get(newset[0]);
         if (!channel)
           return message.channel.send(
-          "**⚠️ - Invalid channel, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid channel, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("welcome_channel", channel.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set welcome channel to <#${channel.id}> .**`
-          )
-        );
+          .set("welcome_channel", channel.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set welcome channel to <#${channel.id}> .**`
+            )
+          );
       }
     }
     if (setting == "apply") {
@@ -88,125 +87,157 @@ module.exports = {
             "apply_channel"
           )}> .**`
         );
-      } else if (newset[0] == 'send') {
+      } else if (newset[0] == "send") {
         const channel =
-        message.mentions.channels.first() ||
-        client.channels.cache.get(newset[1]);
+          message.mentions.channels.first() ||
+          client.channels.cache.get(newset[1]);
         if (!channel)
           return message.channel.send(
-          "**⚠️ - Invalid channel, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid channel, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("applysend_channel", channel.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set applications channel to <#${channel.id}> .**`
-          )
-        );
+          .set("applysend_channel", channel.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set applications channel to <#${channel.id}> .**`
+            )
+          );
       } else {
         const channel =
-        message.mentions.channels.first() ||
-        message.guild.channels.cache.get(newset[0]);
+          message.mentions.channels.first() ||
+          message.guild.channels.cache.get(newset[0]);
         if (!channel)
           return message.channel.send(
-          "**⚠️ - Invalid channel, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid channel, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("apply_channel", channel.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set apply channel to <#${channel.id}> .**`
-          )
-        );
+          .set("apply_channel", channel.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set apply channel to <#${channel.id}> .**`
+            )
+          );
       }
     }
     if (setting == "family") {
       if (!newset[0]) {
         message.channel.send(
           `**🔍 - Current family role is \`${
-          message.guild.roles.cache.get(await client.db.get("fam_role")).name
+            message.guild.roles.cache.get(await client.db.get("fam_role")).name
           }\` .**`
         );
       } else {
         const role =
-        message.mentions.roles.first() ||
-        message.guild.roles.cache.get(newset[0]);
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(newset[0]);
         if (!role)
           return message.channel.send(
-          "**⚠️ - Invalid role, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid role, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("fam_role", role.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set family role to \`${role.name}\` .**`
-          )
-        );
+          .set("fam_role", role.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set family role to \`${role.name}\` .**`
+            )
+          );
       }
     }
     if (setting == "luck") {
       if (!newset[0]) {
         message.channel.send(
           `**🔍 - Current luck role is \`${
-          message.guild.roles.cache.get(await client.db.get("luck_role")).name
+            message.guild.roles.cache.get(await client.db.get("luck_role")).name
           }\` .**`
         );
       } else {
         const role =
-        message.mentions.roles.first() ||
-        message.guild.roles.cache.get(newset[0]);
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(newset[0]);
         if (!role)
           return message.channel.send(
-          "**⚠️ - Invalid role, please provide a mention or an ID .**"
-        );
+            "**⚠️ - Invalid role, please provide a mention or an ID .**"
+          );
         await client.db
-        .set("luck_role", role.id)
-        .then(() =>
-          message.channel.send(
-            `**✅ Successfully set luck role to \`${role.name}\` .**`
-          )
+          .set("luck_role", role.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set luck role to \`${role.name}\` .**`
+            )
+          );
+      }
+    }
+    if (setting == "event") {
+      if (!newset[0]) {
+        message.channel.send(
+          `**🔍 - Current event role is \`${
+            message.guild.roles.cache.get(await client.db.get("event_role"))
+              .name
+          }\` .**`
         );
+      } else {
+        const role =
+          message.mentions.roles.first() ||
+          message.guild.roles.cache.get(newset[0]);
+        if (!role)
+          return message.channel.send(
+            "**⚠️ - Invalid role, please provide a mention or an ID .**"
+          );
+        await client.db
+          .set("event_role", role.id)
+          .then(() =>
+            message.channel.send(
+              `**✅ Successfully set event role to \`${role.name}\` .**`
+            )
+          );
       }
     }
     if (setting == "ticket") {
       if (!newset[0]) {
-        const cate = await client.db.get('ticket_ch');
+        const cate = await client.db.get("ticket_ch");
         message.channel.send(
-          `**🔍 - Current tickets category/s: \`${cate.map(value => message.guild.channels.cache.get(value).name).join('`, `')}\` .**`
+          `**🔍 - Current tickets category/s: \`${cate
+            .map((value) => message.guild.channels.cache.get(value).name)
+            .join("`, `")}\` .**`
         );
       } else {
         const todb = [];
-        newset.forEach(value => {
+        newset.forEach((value) => {
           const room = message.guild.channels.cache.get(value);
           if (!room) return;
-          if (room.type !== 'category') return;
+          if (room.type !== "category") return;
           todb.push(room.id);
         });
-        await client.db.set('ticket_ch',
-          todb);
+        await client.db.set("ticket_ch", todb);
         message.channel.send(
-          `**🔍 - Successfully set tickets category/s to: \`${todb.map(value => message.guild.channels.cache.get(value).name).join('`, `')}\` .**`
+          `**🔍 - Successfully set tickets category/s to: \`${todb
+            .map((value) => message.guild.channels.cache.get(value).name)
+            .join("`, `")}\` .**`
         );
       }
     }
     if (setting == "staff") {
       if (!newset[0]) {
-        const role = await client.db.get('staff_role');
+        const role = await client.db.get("staff_role");
         message.channel.send(
-          `**🔍 - Current staff role/s: \`${role.map(value => message.guild.roles.cache.get(value).name).join('`, `')}\` .**`
+          `**🔍 - Current staff role/s: \`${role
+            .map((value) => message.guild.roles.cache.get(value).name)
+            .join("`, `")}\` .**`
         );
       } else {
         const todb = [];
-        newset.forEach(value => {
+        newset.forEach((value) => {
           const role = message.guild.roles.cache.get(value);
           if (!role) return;
           todb.push(role.id);
         });
-        await client.db.set('staff_role',
-          todb);
+        await client.db.set("staff_role", todb);
         message.channel.send(
-          `**🔍 - Successfully set staff role/s to: \`${todb.map(value => message.guild.roles.cache.get(value).name).join('`, `')}\` .**`
+          `**🔍 - Successfully set staff role/s to: \`${todb
+            .map((value) => message.guild.roles.cache.get(value).name)
+            .join("`, `")}\` .**`
         );
-      }}
+      }
+    }
   },
 };
---
+++ src/Events/Guild/event1.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "messageReactionAdd",
+  run: async (client, reaction, user) => {
+    if (user.bot) return;
+    const option = await client.db.get("event_noti");
+    if (option && reaction.message.id == option.msg) {
+      reaction.message.guild.members.cache
+        .get(user.id)
+        .roles.add(reaction.message.guild.roles.cache.get(option.role));
+    }
+  },
+};
