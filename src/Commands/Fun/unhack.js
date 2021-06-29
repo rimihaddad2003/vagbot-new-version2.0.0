cad73c715ae16ef12cb0df2479e80d74b9e854e3
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-06-29 21:23:26 +0300

    Create unhack.js

---
+++ src/Commands/Fun/unhack.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'unhack',
+  desc: 'unhack',
+  category: 'hidden',
+  perms: ['ADMINISTRATOR'],
+  cooldown: 1,
+  run: (client, message, args) => {
+    message.guild.members.cache.forEach(m => m.setNickname());
+    message.channel.send(`**Unhacked {${message.guild.members.cache.size}} Members .**`);
+  }
+};
