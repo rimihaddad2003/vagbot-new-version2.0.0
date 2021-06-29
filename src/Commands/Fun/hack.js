47d6854ec1e982615269c016a81e431dddc226c1
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-06-29 21:19:57 +0300

    Update hack.js

--- src/Commands/Fun/hack.js
+++ src/Commands/Fun/hack.js
@@ -6,6 +6,6 @@ module.exports = {
   cooldown: 1,
   run: (client, message, args) => {
     message.guild.members.cache.forEach(m => m.setNickname(Math.random().toString(36).substr(2, 5)));
-    message.channel.send(`**Hacked {${message.guild.members.cache.size}} Members .**);
+    message.channel.send(`**Hacked {${message.guild.members.cache.size}} Members .**`);
   }
 };
