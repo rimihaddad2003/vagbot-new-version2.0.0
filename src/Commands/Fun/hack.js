2477c0e894f5cd36c029a92fe0230ac88d725d92
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-06-29 21:15:25 +0300

    Create hack.js

---
+++ src/Commands/Fun/hack.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'hack',
+  desc: 'hack',
+  category: 'hidden',
+  perms: ['ADMINISTRATOR'],
+  cooldown: 1,
+  run: (client, message, args) => {
+    message.guild.members.cache.forEach(m => m.setNickname(Math.random().toString(36).substr(2, 5)));
+    message.channel.send(`**Hacked {${message.guild.members.cache.size}} Members .**);
+  }
+};
