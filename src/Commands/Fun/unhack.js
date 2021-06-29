cd0abca93d6e0cfa6cc066830a3b71192fb6c727
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-06-29 21:26:28 +0300

    Update unhack.js

--- src/Commands/Fun/unhack.js
+++ src/Commands/Fun/unhack.js
@@ -5,7 +5,7 @@ module.exports = {
   perms: ['ADMINISTRATOR'],
   cooldown: 1,
   run: (client, message, args) => {
-    message.guild.members.cache.forEach(m => m.setNickname());
+    message.guild.members.cache.forEach(m => m.setNickname(""));
     message.channel.send(`**Unhacked {${message.guild.members.cache.size}} Members .**`);
   }
 };
