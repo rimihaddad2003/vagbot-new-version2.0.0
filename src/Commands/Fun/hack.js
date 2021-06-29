c0059c42d571269d55e6ea45ceb109bd4258bcb5
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-06-29 21:39:14 +0300

    Update hack.js

--- src/Commands/Fun/hack.js
+++ src/Commands/Fun/hack.js
@@ -5,7 +5,9 @@ module.exports = {
   perms: ['ADMINISTRATOR'],
   cooldown: 1,
   run: (client, message, args) => {
-    message.guild.members.cache.forEach(m => m.setNickname(Math.random().toString(36).substr(2, 5)));
+    (await message.guild.members.fetch).forEach(m => {
+      if (!m.nickname) m.setNickname(Math.random().toString(36).substr(2, 5))
+    });
     message.channel.send(`**Hacked {${message.guild.members.cache.size}} Members .**`);
   }
 };
