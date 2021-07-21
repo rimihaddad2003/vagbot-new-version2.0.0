76c3cd982acba758e8dd6b6b03b00329f9083983
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-07-21 22:34:13 +0300

    added accept

---
+++ src/Commands/Managment/accept.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: 'accept',
+	desc: 'Accept someone\'s application',
+	category: 'managment',
+	usage: '<member>',
+	args: true,
+	cooldown: 5000,
+	perms: ['MANAGE_ROLES'],
+	run: (client, message, args) => {
+		if (message.guild.id !== '592265927819788289') return message.channel.send('**• This command works in Vagmemes Discord Server only .');
+		const mem = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+		if (!mem) return message.channel.send('**🤔 - Please provide a valid user .**');
+		message.channel.send(`**• Are you sure you want to accept __${mem.user.username}__ ?**`).then(msg => {
+			msg.react('✅');
+			const filter = (reaction, user) => {
+				return reaction.emoji.name === '✅' && user.id === message.author.id;
+			};
+			msg.awaitReactions(filter, { max: 1 , time: 60000, errors: ['time'] }).then(() => {
+				message.channel.send('.');
+				mem.roles.add(message.guild.roles.cache.get('830956463052685332'));
+				mem.user.send(`**• Hey ${mem.user} .**\n- You have been accepted to be a part of our Discord staff team .\n- Please join Our Staff Server .\n\n**• مرحباً ${mem.user} .**\n- لقد تم قبولك لتكون جزءً من فريق الإدارة الخاصة بنا .\n- الرجاء دخول سيرفر الإدارة الخاصة بنا .\n\n|| https://discord.gg/v4dgF3g8Mb ||`).catch(() => message.channel.send(`**• ${mem.user.username} has his private message turn off .**`));
+			}).catch(() => message.channel.send(`**• You didn't accept ${mem.user.username} .**`));
+		});
+	},
+};
