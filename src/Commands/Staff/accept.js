47adc79433e15a22984f34d23d22bf93cde285af
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-08-05 13:55:10 +0300

    Update accept.js

--- src/Commands/Staff/accept.js
+++ src/Commands/Staff/accept.js
@@ -1,3 +1,5 @@
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+
 module.exports = {
 	name: 'accept',
 	desc: 'Accept someone\'s application',
@@ -10,17 +12,28 @@ module.exports = {
 		if (message.guild.id !== '592265927819788289') return message.channel.send('**• This command works in Vagmemes Discord Server only .**');
 		const mem = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 		if (!mem) return message.channel.send('**🤔 - Please provide a valid user .**');
-		message.channel.send(`**• Are you sure you want to accept __${mem.user.username}__ ?**`).then(msg => {
-			msg.react('✅');
-			const filter = (reaction, user) => {
-				return reaction.emoji.name === '✅' && user.id === message.author.id;
-			};
-			msg.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] }).then(() => {
-				msg.delete();
-				message.channel.send(`**• The role has been given to __${mem.user.username}__ .**`);
-				mem.roles.add(message.guild.roles.cache.get('830956463052685332'));
-				mem.user.send(`**• Hey ${mem.user} .**\n- You have been accepted to be a part of our Discord staff team .\n- Please join Our Staff Server .\n\n**• مرحباً ${mem.user} .**\n- لقد تم قبولك لتكون جزءاً من فريق الإدارة الخاصة بنا .\n- الرجاء دخول سيرفر الإدارة الخاصة بنا .\n\n|| https://discord.gg/v4dgF3g8Mb ||`).catch(() => message.channel.send(`**• ${mem.user.username} has his private message turn off .**`));
-			}).catch(() => message.channel.send(`**• You didn't accept __${mem.user.username}__ .**`));
+		const button1 = new MessageButton()
+			.setStyle('green')
+			.setLabel('Yes!')
+			.setID('button1');
+		const button2 = new MessageButton()
+			.setStyle('gray')
+			.setLabel('No')
+			.setID('button2');
+		const row = new MessageActionRow()
+			.addComponents(button1, button2);
+		message.channel.send(`**• Are you sure you want to accept __${mem.user.username}__ ?**`, row).then(msg => {
+			const filter = button => button.clicker.user.id == message.author.id;
+			msg.awaitButtons(filter, { max: 1, time: 60000, errors: ['time'] }).then(async collected => {
+				if (collected.first().id == 'button1') {
+					collected.first().reply.send(`**• The role has been given to __${mem.user.username}__ .**`);
+					mem.roles.add(message.guild.roles.cache.get('830956463052685332'));
+					mem.user.send(`**• Hey ${mem.user} .**\n- You have been accepted to be a part of our Discord staff team .\n- Please join Our Staff Server .\n\n**• مرحباً ${mem.user} .**\n- لقد تم قبولك لتكون جزءاً من فريق الإدارة الخاصة بنا .\n- الرجاء دخول سيرفر الإدارة الخاصة بنا .\n\n|| https://discord.gg/v4dgF3g8Mb ||`).catch(() => message.channel.send(`**• ${mem.user.username} has his private message turn off .**`));
+				}
+				else if (collected.first().id == 'button2') {
+					collected.first().reply.send('**Canceled .**');
+				}
+			}).catch(() => message.channel.send(`**⌛ - You didn't accept __${mem.user.username}__ .**`));
 		});
 	},
 };
