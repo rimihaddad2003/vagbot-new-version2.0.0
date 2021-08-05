858e5d7568e15d26bc21273840a6cc3b06feab4a
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-08-05 13:43:17 +0300

    Update unn.js

--- src/Commands/Managment/unn.js
+++ src/Commands/Managment/unn.js
@@ -8,7 +8,6 @@ module.exports = {
 	perms: ['ADMINISTRATOR'],
 	cooldown: 1,
 	run: async (client, message) => {
-		let count = 0;
 		const button1 = new MessageButton()
 			.setStyle('green')
 			.setLabel('Yes!')
@@ -23,12 +22,9 @@ module.exports = {
 			const filter = button => button.clicker.user.id == message.author.id;
 			msg.awaitButtons(filter, { max: 1, time: 60000, errors: ['time'] }).then(async collected => {
 				if (collected.first().id == 'button1') {
-					collected.first().reply.send(`**✅ - UnNicknamed {${count}} Members .**`);
-					await message.guild.members.fetch().filter(m => !m.user.bot && m.nickname).then(f => f.forEach(m => {
-						m.setNickname('');
-						count++;
-					}),
-					);
+					const mems = await message.guild.members.fetch().then(f => f.filter(m => !m.user.bot && m.nickname && m.roles.highest.position < message.guild.me.roles.highest.position));
+					collected.first().reply.send(`**✅ - UnNicknamed {${mems.size}} Members .**`);
+					mems.forEach(m => m.setNickname(''));
 				}
 				else if (collected.first().id == 'button2') {
 					collected.first().reply.send('**Canceled .**');
