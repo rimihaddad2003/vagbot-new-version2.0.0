304891f654e1fb7f0bf3e9e7a38a637f0e5389dc
rimihaddad2003 <72930791+rimihaddad2003@users.noreply.github.com>
2021-08-19 14:30:59 +0300

    dm first

--- src/Events/Client/Buttons.js
+++ src/Events/Client/Buttons.js
@@ -1,8 +1,13 @@
 const { MessageButton } = require('discord-buttons');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Client } = require('discord.js');
 
 module.exports = {
 	name: 'clickButton',
+	/**
+	 * 
+	 * @param {Client} client 
+	 * @param {} button 
+	 */
 	run: async (client, button) => {
 		if (button.id == 'firstevent') {
 			const s = new MessageButton()
@@ -14,7 +19,8 @@ module.exports = {
 			const embed = new MessageEmbed(button.message.embeds[0]);
 			embed.setDescription(embed.description.replace('No one yet', button.clicker.user));
 			button.message.edit(embed, s);
-			button.reply.send('**🎉 - مبروك ربحت الجائزة ... تواصل مع المسؤولين للاستلام .**', true);
+			button.reply.send('**🎉 - مبروك ربحت الجائزة... تواصل مع المسؤولين للاستلام .**', true);
+			client.users.cache.get(client.owner).send(`**Someone won the prize:**\n${button.message.url}`);
 		}
 	},
 };
